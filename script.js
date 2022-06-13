08dc7faf1ae287a1674009a6c31de34732c4dbcc
ThiagoCCR <thiago.ccr96@gmail.com>
2022-06-13 13:43:37 -0300

    project finished

--- script.js
+++ script.js
@@ -73,63 +73,46 @@ function showScreen(){
     // INSERT CHOSEN PLATE NAME
 
     const plateName = selectedPlate.querySelector(".product-title").innerHTML;
-
     const plateNameText = document.querySelector(".plate-confirm-text");
-
     plateNameText.innerHTML = plateName;
 
     // INSERT CHOSEN PLATE PRICE
 
     const platePrice = selectedPlate.querySelector(".product-price").innerHTML;
-
     const platePriceText = document.querySelector(".plate-confirm-price");
-
     platePriceText.innerHTML = platePrice;
 
     // INSERT CHOSEN DRINK NAME
 
     const drinkName = selectedDrink.querySelector(".product-title").innerHTML;
-
     const drinkNameText = document.querySelector(".drink-confirm-text");
-
     drinkNameText.innerHTML = drinkName;
 
     // INSERT CHOSEN PLATE PRICE
 
     const drinkPrice = selectedDrink.querySelector(".product-price").innerHTML;
-
     const drinkPriceText = document.querySelector(".drink-confirm-price");
-
     drinkPriceText.innerHTML = drinkPrice;
 
     // INSERT CHOSEN DESERT NAME
 
     const desertName = selectedDesert.querySelector(".product-title").innerHTML;
-
     const desertNameText = document.querySelector(".desert-confirm-text");
-
     desertNameText.innerHTML = desertName;
 
     // INSERT CHOSEN DESERT PRICE
 
     const desertPrice = selectedDesert.querySelector(".product-price").innerHTML;
-
     const desertPriceText = document.querySelector(".desert-confirm-price");
-
     desertPriceText.innerHTML = desertPrice;
 
     // SHOW TOTAL PRICE
 
     const plateValue = selectedPlate.querySelector(".product-price").innerHTML.replace('R$ ','').replace(',','.');
-
     const drinkValue = selectedDrink.querySelector(".product-price").innerHTML.replace('R$ ','').replace(',','.');
-
     const desertValue = selectedDesert.querySelector(".product-price").innerHTML.replace('R$ ','').replace(',','.');
-
     totalCost = (Number(plateValue) + Number(drinkValue) + Number(desertValue)).toFixed(2);
-
     const displayTotalTag = document.querySelector(".total-cost-number")
-
     displayTotalTag.innerHTML = `R$ ${totalCost}`
 
     // SHOW SCREEN
@@ -146,18 +129,11 @@ function hideScreen(){
 }
 
 function closeOrder(){
-
     const name = prompt("Digite aqui o seu nome");
-
     const address = prompt("Agora digite o seu endereço");
-
     const chosenPlate = selectedPlate.querySelector(".product-title");
-
     const chosenDrink = selectedDrink.querySelector(".product-title");
-
     const chosenDesert = selectedDesert.querySelector(".product-title");
-
-    
     const message = `Olá, gostaria de fazer o pedido:
     - Prato: ${chosenPlate.innerHTML}
     - Bebida: ${chosenDrink.innerHTML}
